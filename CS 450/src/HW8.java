5e395fabc0af2730484d76eb43c54e32cb20e711
trevorab <supraaddict3@gmail.com>
2014-03-26 20:33:49 -0600

    Part 2 of Lab 8 Completed - Shrink Image

--- CS 450/src/HW8.java
+++ CS 450/src/HW8.java
@@ -81,10 +81,10 @@ public class HW8 {
         int width = img.getWidth();
         int height = img.getHeight();
 
-        int factor = Integer.parseInt(CS450.prompt("Enter the multiplication factor you'd like to use"));
+        int factor = Integer.parseInt(CS450.prompt("Enter the shrinking factor you'd like to use"));
         float factorFloat = (float)factor;
-        int newWidth = img.getWidth() * factor;
-        int newHeight = img.getHeight() * factor;
+        int newWidth = img.getWidth() / factor;
+        int newHeight = img.getHeight() / factor;
 
         BufferedImage newImg = new BufferedImage(newWidth, newHeight, BufferedImage.TYPE_INT_RGB);
         WritableRaster newImgRaster = newImg.getRaster();
@@ -92,33 +92,27 @@ public class HW8 {
         for(int x = 0; x < newWidth; x++) {
             for(int y = 0; y < newHeight; y++) {
 
-                float xPos = x / factorFloat;
-                float yPos = y / factorFloat;
+                float xPos = x * factorFloat;
+                float yPos = y * factorFloat;
 
-                int x0 = (int)(xPos - (xPos % 1));
-                int x1 = x0 + 1;
+                float interpolatedValue = 0;
 
-                if(x1 >= width) {
-                    x1 = width - 1;
+                float xDivisionValue = factorFloat;
+                float yDivisionValue = factorFloat;
+                if(x == 0) {
+                    xDivisionValue /= factorFloat;
+                }
+                if(y == 0) {
+                    yDivisionValue /= factorFloat;
                 }
 
-                int y0 = (int)(yPos - (yPos % 1));
-                int y1 = y0 + 1;
-
-                if(y1 >= height) {
-                    y1 = height - 1;
+                // Average (interpolate equally) all the pixels that this new pixel now covers
+                float divisionValue = xDivisionValue * yDivisionValue;
+                for(int xP = 0; xP < xDivisionValue; xP++) {
+                    for(int yP = 0; yP < yDivisionValue; yP++) {
+                        interpolatedValue += imgRaster.getSample((int)xPos - xP, (int)yPos - yP, 0) / divisionValue;
+                    }
                 }
-
-                float x0Weight = (x1 - xPos);
-                float x1Weight = (xPos - x0);
-
-                float lowerInterpolation  = x0Weight * imgRaster.getSample(x0, y0, 0) + x1Weight * imgRaster.getSample(x1, y0, 0);
-                float higherInterpolation = x0Weight * imgRaster.getSample(x0, y1, 0) + x1Weight * imgRaster.getSample(x1, y1, 0);
-
-                float y0Weight = (y1 - yPos);
-                float y1Weight = (yPos - y0);
-
-                float interpolatedValue = y0Weight * lowerInterpolation + y1Weight * higherInterpolation;
 
                 newImgRaster.setSample(x, y, 0, interpolatedValue);
                 newImgRaster.setSample(x, y, 1, interpolatedValue);
